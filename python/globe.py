84f90e357caac3f5b0eca0b8b3bb26d17ef997df
ritabratamoitra <ritabrata1808@live.com>
2022-04-09 22:51:44 +0530

    Add beta distribution as prior

--- python/globe.py
+++ python/globe.py
@@ -1,18 +1,26 @@
-import numpy as np
-from scipy.stats import binom
-import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import binom, beta
 
 
 def globe():
     grid = np.arange(0, 1, 0.001)
-    prior = np.ones(1000)
 
+    uniform_prior = np.ones(1000)
+    plot_posterior(grid, uniform_prior)
+
+    beta_prior = beta.ppf(grid, 3, 2)
+    plot_posterior(grid, beta_prior)
+
+    plt.show()
+
+
+def plot_posterior(grid, prior):
     p_data = binom.pmf(k=6, n=9, p=grid)
+
     posterior = p_data * prior
     posterior_normalised = posterior / sum(posterior)
     plt.plot(posterior_normalised)
-    plt.show()
 
 
 if __name__ == '__main__':
